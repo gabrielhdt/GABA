ba5e1849a6d2816206fadb42b7fa8ec5943c74e5
gabrielhdt <gabrielhondet@gmail.com>
2017-04-07 21:56:48 +0200

    strings to heredoc, new func
    Added add_followed function and passed strings to heredoc

--- db.php
+++ db.php
@@ -5,22 +5,41 @@ $username = "gaba";
 $dbname = "IENAC_GABA";
 $password = "abag";
 
-function insert_species($name, $kingdom, $phylum, $class, $order, $family,
+function add_species($name, $kingdom, $phylum, $class, $order, $family,
     $conservation_status) {
     global $servername, $username, $dbname, $password;
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $query = "INSERT INTO Species (name, kingdom, phylum, class, order_s, ";
-        $query .= "family, conservation_status, footprint) ";
-        $query .= "VALUES ('$name', '$kingdom', '$phylum', '$class', '$order', ";
-        $query .= "'$family', '$conservation_status', NULL);";
-
+        $query = <<<QRY
+INSERT INTO Species (name, kingdom, phylum, class, order_s, family,
+conservation_status, footprint)
+VALUES ($name, $kingdom, $phylum, $class, $order, $family, $conservation_status,
+NULL);
+QRY;
         $conn->exec($query);
     } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo "Insertion failed: " . $e->getMessage();
     }
     $conn = null;
+}
+
+function add_followed($idSpecies, $gender, $health, $idFacility = NULL,
+    $birth = NULL, $death = NULL)
+{
+    global $servername, $username, $dbname, $password;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $query = <<<QRY
+INSERT INTO Followed (gender, birth, death, health, idSpecies, idFacility)
+VALUES ($gender, $birth, $death, $health, $idSpecies, $idFacility);
+QRY;
+        $conn->exec($query);
+    } catch (PDOException $e) {
+        echo "Insertion failed: " . $e->getMessage();
+    }
 }
 ?>
