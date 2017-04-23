9c19fbb6c65020e13d2614364a12a209e6f5cb24
gabrielhdt <gabrielhondet@gmail.com>
2017-04-23 17:36:51 +0200

    added program to upload pictures

---
+++ upload_pic.php
@@ -0,0 +1,22 @@
+<?php
+/* POST must contain 'table', which specifies what is the image about
+ * (e.g. followed or species) and 'id' of the thing; fields which won't be
+ * entered by the user, and therefore parameters must be passed via hidden
+ * input in forms
+ * http://php.net/manual/en/features.file-upload.post-method.php
+ * Input file must be a picture (managed in input of form, see
+ * musicbrainz for example)
+ */
+$picprop = getimagesize($_FILES['userpic']['tmp_name']);
+$ext = $picprop['mime'];
+$fname = mb_substr($_POST['table'], 0, 2) . $_POST['id'] . ".$ext";
+$uploaddir = "/var/www/html/data/pics/";
+$uploadfile = $uploaddir . basename($fname);
+
+if (move_uploaded_file($_FILES['userpic']['tmp_name'], $uploadfile)) {
+    echo "Picture is valid, and successfully uploaded.\n";
+}
+else {
+    echo "Possible file upload attack.\n";
+}
+?>
