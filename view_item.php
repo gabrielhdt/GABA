f01c7d78d08c081ce9ded0297effd6aa38f75efc
gabrielhdt <gabrielhondet@gmail.com>
2017-04-25 15:56:59 +0200

    using templates

--- view_item.php
+++ view_item.php
@@ -1,26 +1,8 @@
 <!DOCTYPE HTML>
 <html>
-<?php include "db.php"; ?>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width = device-width, initial-scale = 1">
-    <title>GABA V1</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css"
-        type="text/css" charset="utf-8">
-    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"
-        integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
-        crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"
-         integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg=="
-         crossorigin=""></script>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-
+<?php include "db.php";
+include "head.php";
+?>
 <body>
 <form action="view_item.php" method="post" accept-charset="utf-8"
     enctype="multipart/form-data">
@@ -78,8 +60,7 @@ if (array_key_exists('search_field', $_POST))
     echo '</table>';
 }
 ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<?php include "footer.php"; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table-locale-all.min.js"
     type="text/javascript" charset="utf-8"></script>
 </body>
