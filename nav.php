13efe4c7979f02dd74218bab343acbfbcb5ac32b
Aurélie <bornot.a@gmail.com>
2017-04-25 14:32:00 +0200

    dropdown

--- nav.php
+++ nav.php
@@ -17,7 +17,7 @@ Dropdown
 <span class="caret"></span>
 </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                            <li><a href="#">Action</a></li>
+                            <li><a href="#">djhegdjyfe</a></li>
                             <li><a href="#">Another action</a></li>
                             <li><a href="#">Something else here</a></li>
                             <li role="separator" class="divider"></li>
