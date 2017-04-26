0d05c4f87e972a5b77594f3f35ec1472df69e114
ViryBe <benoit.viry@laposte.net>
2017-04-26 20:17:33 +0200

    page de login sans css

--- login.php
+++ login.php
@@ -9,13 +9,11 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') { // test 
             $_SESSION['login'] = $_POST['login'];
             header('Location: membre_index.php'); // redirection vers la 'page index de session'
             exit();
-        }
-        else {
+        } else {
             $erreur = 'Login ou mot de passe incorrect';
         }
-    }
-    else {
-    $erreur = 'Au moins un des champs est vide.';
+    } else {
+        $erreur = 'Au moins un des champs est vide.';
     }
 }
 ?>
@@ -27,13 +25,13 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') { // test 
     <body>
         Connexion à l'espace membre :<br />
         <?php //afficher une éventuelle erreur
-        if (isset($erreur)) echo '<br /><br />',$erreur;
+            if (isset($erreur)) echo '<br /><br />',$erreur;
         ?>
         <form action="index.php" method="post" autofocus>
             <input type="text" name="login"
-            value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>" required>
+                value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>" required>
             <input type="password" name="password"
-            value="<?php if (isset($_POST['password'])) echo htmlentities(trim($_POST['password'])); ?>" required>
+                value="<?php if (isset($_POST['password'])) echo htmlentities(trim($_POST['password'])); ?>" required>
             <input type="submit" name="connexion" value="Connexion">
         </form>
     </body>
