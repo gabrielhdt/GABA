5457d57e37e41d3e26715a842758d054894c8150
ViryBe <benoit.viry@laposte.net>
2017-04-28 16:14:47 +0200

    page recherche

---
+++ recherche.php
@@ -0,0 +1,38 @@
+<html>
+
+    <?php
+    include 'head.php';
+    include 'nav.php';
+    ?>
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+    </style>
+
+    <body>
+        <div class="container-fluid">
+                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                    <a href="espece.php">
+                        <div id="espece">Espèce</div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                    <a href="individu.php">
+                        <div id="individu">Individu</div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                    <a href="batiment.php">
+                        <div id="batiment">Bâtiment</div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+                    <a href="chercheur.php">
+                        <div id="chercheur">Chercheur</div>
+                    </a>
+            </div>
+            </div>
+
+
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
