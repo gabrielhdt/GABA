4f81907a5aff36a39d52a33ce00218b9daac9568
AdrienLcn <a.lancelon@gmail.com>
2017-04-28 19:21:10 +0200

    saisie <- addfollowed + version php +addspecies html&phph +modif css container3, container4

--- addspecies.php
+++ addspecies.php
@@ -2,22 +2,28 @@
 <html lang="fr">
 <?php include 'db.php' ?>
 <?php include "head.php" ?>
+
+
 <body>
 <?php include "nav.php" ?>
 
-<div class="container3">
-    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-6 col-md-offset-6 col-sm-offset-4">
+<div class="container4">
+    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
         <div class="description">
             <div class="form">
                 <div id="contact">
-                    <h1>Ajouter un individu</h1>
+                    <h1>Ajouter une espèce</h1>
                     <p>Remplissez le formulaire ci-dessous pour compléter notre base de donnée.</p>
                     <p>Chaque contribution nous permet de vous offrir un service de meilleur qualité.</p>
                     <form action="addfollowed.html" method="post">
-                        <input type="text" name="species" placeholder="Espèce*">
-                        <input type="text" name="gender" placeholder="Sexe*">
-                        <input type="text" name="birth" placeholder="Date de naissance*">
-                        <input type="text" name="health" placeholder="Etat de santé*">
+                        <input type="text" name="species" placeholder="Nom de l'espèce*">
+                        <input type="text" name="kingdom" placeholder="Royaume*">
+                        <input type="text" name="phylum" placeholder="Phylum*">
+                        <input type="text" name="class" placeholder="Classe*">
+                        <input type="text" name="order" placeholder="Ordre*">
+                        <input type="text" name="family" placeholder="Famille*">
+                        <input type="text" name="genus" placeholder="Genus*">
+                        <input type="text" name="status" placeholder="pwet*status">
                         <button class="btn btn-success" type="submit" name="submit_contact">Enregistrer</button>
                     </form>
                 </div>
@@ -28,8 +34,8 @@
 
 <br><br><br>
 
+<?php include "footer.php" ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<?php include "footer.php" ?>
 </body>
 </html>
