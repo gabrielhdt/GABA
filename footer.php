69c02b9f16acba20356f667e2289ac84ff9115b8
ViryBe <benoit.viry@laposte.net>
2017-04-28 15:02:54 +0200

    nouveau footer

--- footer.php
+++ footer.php
@@ -1,19 +1,18 @@
 <footer>
     <ul>
-        <li><a href="#">Accueil</a> | </li>
-        <li><a href="#">Notre labo</a> | </li>
-        <li>Recherche :
-            <ul>
-                <li><a href="#">Espèces</a></li>
-                <li><a href="#">Individus</a></li>
-                <li><a href="#">Bâtiments</a></li>
-                <li><a href="#">Chercheurs</a></li>
-            </ul>
-        </li>
-        <li><a href="#contact">Contact</a> | </li>
-        <li><a href="#">Help</a> | </li>
-        <li><a href="#">Connexion</a></li>
+        <li class="active"><a href="#">Accueil</a></li>|
+        <li><a href="#">Notre labo</a></li>|
+        <li><a href="#">Espèce</a></li>|
+        <li><a href="#">Individu</a></li>|
+        <li><a href="#">Bâtiment</a></li>|
+        <li><a href="#">Chercheur</a></li>|
+        <li><a href="#">Help</a></li>
     </ul>
+    &copy All rights reserved GABA
+    <br>
+    <div class="footer-up">
+        <a href="#mynav">Up <span class="glyphicon glyphicon-chevron-up"></span></a>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </footer>
