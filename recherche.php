9cc6adbdcf130c7a8e7cb852a8917648a3193f46
ViryBe <benoit.viry@laposte.net>
2017-04-29 13:23:17 +0200

    page ok

--- recherche.php
+++ recherche.php
@@ -10,22 +10,12 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">
-                    <a href="espece.php">Espèce</a>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">
-                    <a href="individu.php">Individu</a>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">
-                    <a href="batiment.php">Bâtiment</a>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">
-                    <a href="chercheur.php">Chercheur</a>
-                </div>
+                    <a href="espece.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Espèce</div></a>
+                    <a href="individu.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Individu</div></a>
+                    <a href="batiment.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Bâtiment</div></a>
+                    <a href="chercheur.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Chercheur</div></a>
             </div>
         </div>
-
-
         <?php include 'footer.php'; ?>
     </body>
 </html>
