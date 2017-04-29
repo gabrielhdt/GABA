0ed5e296e98ff53729d69144acd95900aa8b7a73
gabrielhdt <gabrielhondet@gmail.com>
2017-04-29 09:57:37 +0200

    select forms, bit of php, to be continued

--- addfollowed.php
+++ addfollowed.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
-<?php include 'db.php' ?>
-<?php include "head.php" ?>
+<?php include 'db.php';
+include 'forms_fct.php';
+include "head.php"; ?>
 <body>
 <?php include "nav.php" ?>
 
@@ -9,13 +10,20 @@
     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-6 col-md-offset-6 col-sm-offset-4">
         <div class="description">
             <div class="form">
-                <div id="add_followed">
+                <div id="contact">
                     <h1>Ajouter un individu</h1>
                     <p>Remplissez le formulaire ci-dessous pour compléter notre base de donnée.</p>
                     <p>Chaque contribution nous permet de vous offrir un service de meilleur qualité.</p>
                     <form action="addfollowed.php" method="post">
-                        <input type="text" name="species" placeholder="Espèce*">
+                        <select name="species" class="form-control">
+                            <option value='1'>Lynx Rufus</option>
+                        </select>
                         <input type="text" name="gender" placeholder="Sexe*">
+                        <select name="gender" class="form-control">
+                            <option value='m'>Male</option>
+                            <option value='f'>Female</option>
+                            <option value='h'>Hermaphrodite</option>
+                        </select>
                         <input type="date" name="birth" placeholder="Date de naissance*">
                         <input type="text" name="health" placeholder="Etat de santé*">
                         <button class="btn btn-success" type="submit" name="add_followed">Enregistrer</button>
