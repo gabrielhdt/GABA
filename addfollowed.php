5734d1a0adbc066d87b68ba52cbbc339682dd625
gabrielhdt <gabrielhondet@gmail.com>
2017-04-29 09:36:29 +0200

    add function, modified a bit form

--- addfollowed.php
+++ addfollowed.php
@@ -9,17 +9,27 @@
     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-6 col-md-offset-6 col-sm-offset-4">
         <div class="description">
             <div class="form">
-                <div id="contact">
+                <div id="add_followed">
                     <h1>Ajouter un individu</h1>
                     <p>Remplissez le formulaire ci-dessous pour compléter notre base de donnée.</p>
                     <p>Chaque contribution nous permet de vous offrir un service de meilleur qualité.</p>
-                    <form action="addfollowed.html" method="post">
+                    <form action="addfollowed.php" method="post">
                         <input type="text" name="species" placeholder="Espèce*">
                         <input type="text" name="gender" placeholder="Sexe*">
-                        <input type="text" name="birth" placeholder="Date de naissance*">
+                        <input type="date" name="birth" placeholder="Date de naissance*">
                         <input type="text" name="health" placeholder="Etat de santé*">
-                        <button class="btn btn-success" type="submit" name="submit_contact">Enregistrer</button>
+                        <button class="btn btn-success" type="submit" name="add_followed">Enregistrer</button>
                     </form>
+<?php
+if (isset($_POST['species']))
+{
+    add_line('Followed',
+        array('idSpecies' => $_POST['species'],
+        'gender' => $_POST['gender'],
+        'birth' => $_POST['birth'],
+        'health' => $_POST['health']));
+}
+?>
                 </div>
             </div>
         </div>
