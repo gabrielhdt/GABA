d713e745b336d38de87ecae906f7054c254aaa70
gabrielhdt <gabrielhondet@gmail.com>
2017-04-29 09:57:23 +0200

    added comment

--- search_followed.php
+++ search_followed.php
@@ -36,6 +36,7 @@ function create_choice_list($disp_fields)
 {
     /* Creates a choice list
      * disp_fields array (column name => displayed name)
+     * Doesn't include the <select name= id= class=> </select>
      */
     foreach ($disp_fields as $col => $disp)
     {
