a65672e83c60819f8a26cd7c7e58b01c22f3d1a6
ViryBe <benoit.viry@laposte.net>
2017-04-30 20:31:01 +0200

    j'en ai marre

--- admin_index.php
+++ admin_index.php
@@ -3,68 +3,11 @@
 // if (!isset($_SESSION['login']) && $_SESSION['login'] != "admin") { // test si l'utilisateur est bien passé par le formulaire
 //     header ('Location: login.php'); // sinon retour page login
 //     exit();
-// }
-include('db.php');
-if (isset($_POST['submit_contact']) && $_POST['submit_contact'] == 'Enregistrer') {
-    if (isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['pwd1']) && !empty($_POST['pwd1']) &&
-        isset($_POST['prenom']) && !empty($_POST['prenom']) && isset($_POST['pwd2']) && !empty($_POST['pwd2'])) {
-
-        $pwd_test = $_POST['pwd1'] == $_POST['pwd2'];
-
-        if ($pwd_test) { // si ok -> next page
-            add_staff($_POST['pwd1'], $_POST['pwd1'], $_POST['nom'], $_POST['prenom']);
-            $msg = "Staff ajouté !";
-        }
-        elseif (!$pwd_test) {
-            $msg = "Erreur !"; // debut de la session
-        }
-    }
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
 <?php include "head.php" ?>
-<style>
-    #msg {
-        background-color: lightgrey;
-        padding: 20px;
-        border-radius: 10px;
-        margin: 15px;
-    }
-    #messages {
-        border-left: 2px solid black;
-        border-top: 1px solid rgb(150, 150, 150);
-    }
-    #messages h1 {
-        text-align: center;
-    }
-    #add_staff {
-        text-align: center;
-        padding: 20px 0;
-        border-top: 1px solid rgb(150, 150, 150);
-    }
-    #add_staff input[name='nom'],
-    #add_staff input[name='prenom'],
-    #add_staff input[name='pwd1'],
-    #add_staff input[name='pwd2'],
-    #add_staff input[name='submit_contact'] {
-        margin: 15px 0;
-    }
-    #add_staff h1{
-        padding-bottom: 35px;
-    }
-    #add_staff {
-        padding: 0 15px;
-    }
-    #add_staff form button{
-        margin: 15px 0;
-    }
-    #add_staff p {
-        color : red;
-    }
-</style>
 <body>
     <?php include "nav.php" ?>
     <div class="container-fluid">
@@ -72,29 +15,25 @@ if (isset($_POST['submit_contact']) && $_POST['submit_contact'] == 'Enregistrer'
 
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="add_staff">
                 <h1>Ajout de personnel :</h1>
-                <form action="admin_index.php" method="post">
+                <form>
                     <p><?php if (isset($msg)){echo $msg;} ?></p>
-                    <input class='form-control' name="nom" placeholder="Nom" required>
-                    <input class='form-control' name="prenom" placeholder="Prenom" required>
-                    <input class='form-control' name="pwd1" placeholder="Mot de passe" required>
-                    <input class='form-control' name="pwd2" placeholder="Confirmation mot de passe" required>
-                    <label class="checkbox-inline"><input type="checkbox" value="vet">Veterinaire</label>
-                    <label class="checkbox-inline"><input type="checkbox" value="">Chercheur</label>
-                    <label class="checkbox-inline"><input type="checkbox" value="">Technicien</label><br>
-                    <input class="btn btn-success" type="submit" name="submit_contact" value="Enregistrer">
+                    <input class='form-control' name="nom" placeholder="Nom">
+                    <input class='form-control' name="prenom" placeholder="Prenom">
+                    <input class='form-control' id="p1" type="password" name="pwd1" placeholder="Mot de passe">
+                    <input class='form-control' id="p2" type="password" name="pwd2" placeholder="Confirmation mot de passe">
+                    <label class="radio-inline"><input type="radio" name="typeStaff" value="vet">Veterinaire</label>
+                    <label class="radio-inline"><input type="radio" name="typeStaff" value="cher">Chercheur</label>
+                    <label class="radio-inline"><input type="radio" name="typeStaff" value="tech">Technicien</label><br>
+                    <input onclick="myTest()" class="btn btn-success" name="submit_contact" value="Enregistrer">
                 </form>
             </div>
 
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="messages">
                 <h1>Messages :</h1>
                 <div id="msg">
-                    Nom :
+                    <a id="del" href="#" title="delete"><span class="glyphicon glyphicon-remove"></span></a>
+                    Date :
                     <hr>
-                    E-mail :
-                    <hr>
-                    Message :
-                </div>
-                <div id="msg">
                     Nom :
                     <hr>
                     E-mail :
@@ -105,4 +44,22 @@ if (isset($_POST['submit_contact']) && $_POST['submit_contact'] == 'Enregistrer'
         </div>
     </div>
     <?php include 'footer.php'; ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        function myTest(){
+            $.post(
+                'addStaff_script.php',
+                {   nom: $("input[name=nom]").val(),
+                    prenom: $("input[name=prenom]").val(),
+                    typeStaff: $("input[name=typeStaff]").val(),
+                    pwd1: $("input[name=pwd1]").val(),
+                    pwd2 : $("input[name=pwd2]").val()},
+
+                function(returnedData) {
+                    alert(returnedData);
+                }
+            );
+            // alert(p1);
+        }
+    </script>
 </body>
