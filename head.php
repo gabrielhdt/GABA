c26f9938e49e263d4591f649c8971bbb7a89b9bd
gabrielhdt <gabrielhondet@gmail.com>
2017-04-30 13:57:27 +0200

    jquery call in head

--- head.php
+++ head.php
@@ -12,5 +12,6 @@
     <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"
          integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg=="
          crossorigin=""></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
