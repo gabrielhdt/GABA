4076d2b585a9b56122aee35931c0a8df1b29bb27
gabrielhdt <gabrielhondet@gmail.com>
2017-05-04 14:24:26 +0200

    added scriptfile used for adding followed

---
+++ script/add_script.php
@@ -0,0 +1,15 @@
+<?php
+include "db.php";
+
+$geostack = array();
+if (isset($_POST['geoloc']) && !empty($_POST['geoloc']))
+{
+    array_push($geostack, $_POST['geoloc']);
+}
+
+function get_geoloc()
+{
+    /* Get last entered geo location */
+    return array_pop($geostack);
+}
+?>
