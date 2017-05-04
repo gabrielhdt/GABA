7d00c890e280f110111992a4ec3687dcd15ce870
gabrielhdt <gabrielhondet@gmail.com>
2017-05-04 18:32:25 +0200

    corrected error script -> js

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"
 integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg=="
 crossorigin=""></script>
-<script src="script/myScript.js" type="text/javascript"></script>
+<script src="js/myScript.js" type="text/javascript"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     $(window).scroll(function() {
