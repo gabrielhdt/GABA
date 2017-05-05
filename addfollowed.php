d77494267881fef0476201dbc0ab00e37f9a8182
gabrielhdt <gabrielhondet@gmail.com>
2017-05-05 09:04:49 +0200

    use get_whereplus

--- addfollowed.php
+++ addfollowed.php
@@ -10,12 +10,12 @@ include "script/add_script.php"
 <?php include "nav.php" ?>
 <?php
 $id_biname = array();
-$lines = get_values(array('idSpecies', 'binomial_name'), 'Species');
+$lines = get_whereplus(array('idSpecies', 'binomial_name'), 'Species');
 foreach ($lines as $line)
 {
     $id_biname[$line['idSpecies']] = $line['binomial_name'];
 }
-$lines = get_values(array('idFacility', 'name'), 'Facility');
+$lines = get_whereplus(array('idFacility', 'name'), 'Facility');
 $id_faname = array();
 foreach ($lines as $line)
 {
