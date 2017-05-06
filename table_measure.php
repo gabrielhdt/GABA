5d09c8cd7725304d18a642fe052845254c3942bf
ViryBe <benoit.viry@laposte.net>
2017-05-06 19:44:06 +0200

    - 1 ligne

--- table_measure.php
+++ table_measure.php
@@ -10,7 +10,7 @@ include 'db.php';
 
 function table_type($idFollowed, $type){
     $result = get_values(
-        array('DISTINCT MiscQuantity.value, MiscQuantity.unit, Measure.date_measure'),
+        array('MiscQuantity.value, MiscQuantity.unit, Measure.date_measure'),
         'MiscQuantity INNER JOIN Measure ON MiscQuantity.idMeasure = Measure.idMeasure',
         $where=array(array('binrel' => '=', 'field' => 'Measure.idFollowed', 'value' =>  "$idFollowed", 'type' => PDO::PARAM_STR),
                      array('binrel' => '=', 'field' => 'MiscQuantity.type', 'value' =>  "$type", 'type' => PDO::PARAM_STR))); // + and id =, order by date_measure
@@ -35,7 +35,6 @@ function table_type($idFollowed, $type){
     }
     $tableType .= "</tr>\n</table>";
     echo $tableType;
-
 }
 
 function table ($idFollowed){
