0ca5bc3e405d78f38dabfc13ede491db9feb36c9
ViryBe <benoit.viry@laposte.net>
2017-05-07 18:58:30 +0200

    names facility, species

--- table_followed_test.php
+++ table_followed_test.php
@@ -14,16 +14,22 @@ function type_measure ($idFollowed) {
 }
 
 function info_followed_table ($id) {
+    $from = "Followed INNER JOIN Species ON Species.idSpecies = Followed.idSpecies INNER JOIN Facility ON Facility.idFacility";
     $where['str'] = 'idFollowed=?';
     $where['valtype'] = array(array('value' => $id, 'type' => PDO::PARAM_INT));
-    $infos = get_values_light("gender, birth, death, health, idSpecies, idFacility, annotation",
-    "Followed", $where);
+    $infos = get_values_light("gender, birth, death, health, Species.binomial_name, Facility.name, annotation",
+    $from, $where);
     $table = "<table>\n";
     foreach ($infos[0] as $key => $value) {
         $table .= "<tr><td>$key</td><td>".($value ? $value : 'null')."</td></tr>\n";
     }
     $table .= "</table>\n";
     echo $table;
+}
+
+function last_measure($id, $measure) {
+    $where['str'] = 'idFollowed=? AND ';
+    $where['valtype'] = array(array('value' => $id, 'type' => PDO::PARAM_INT));
 }
 
 info_followed_table(14);
