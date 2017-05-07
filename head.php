d2a873c8943bb058d8cd9885cd973e3b96348e5c
ViryBe <benoit.viry@laposte.net>
2017-05-07 22:06:01 +0200

    .svg

--- head.php
+++ head.php
@@ -10,4 +10,4 @@
     crossorigin=""/>
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/nav_size.css">
-<link rel="icon" href="image/icon.png">
+<link rel="icon" href="image/icon.svg">
