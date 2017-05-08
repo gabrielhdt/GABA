04f637725b0144c83dc4b54998f94856e8465339
ViryBe <benoit.viry@laposte.net>
2017-05-08 14:31:35 +0200

    links center

--- membre_index.php
+++ membre_index.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['login'])) { // test si l'utilisateur est bien passé par l
     <body>
         <div class="container-fluid">
             <div id="add" class="row">
-                    <a href="addspecies.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Ajout d'une espèce</div></a>
+                    <a href="addspecies.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Ajout d'une espèce</a>
                     <a href="addfollowed.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Ajout d'un individu</div></a>
                     <a href="addfacility.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Ajout d'un bâtiment</div></a>
                     <a href="perso.php"><div class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Informations personnelles</div></a>
