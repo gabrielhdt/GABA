6f29170026b37fef0bfbdf6c09fb3c6b6e4fda77
ViryBe <benoit.viry@laposte.net>
2017-05-08 20:59:18 +0200

    en .php, le top

--- index.php
+++ index.php
@@ -8,13 +8,19 @@
 
 <body>
     <?php include 'nav.php'; ?>
-    <div class="container">
-        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-7 col-md-offset-7 col-sm-offset-5">
+    <div id="outer" class="container">
+        <div id="middle" class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-7 col-md-offset-7 col-sm-offset-5">
             <div class="description">
-                <h1>A propos de nous ...</h1>
-                <p>Le laboratoire science exchange a pour mission de regrouper les informations de multiples laboratoires de recherche animale et de mettre à disposition les données sur nottre plateforme. <br>L'objectif est de faciliter le travail des chercheurs
-                    mais le site permet aussi d'assouvir votre curiosité de manière ludique ! </p>
-                <div id="more"><button type="button" class="btn btn-success"><a href="nous.php">En savoir plus</a></button></div>
+                <div class="middle">
+                    <div class="inner">
+                        <h1>A propos de nous ...</h1>
+                        <p>Le laboratoire science exchange a pour mission de regrouper les informations de multiples laboratoires de recherche animale et de mettre à disposition les données sur nottre plateforme. <br>L'objectif est de faciliter le travail
+                            des chercheurs mais le site permet aussi d'assouvir votre curiosité de manière ludique ! </p>
+                        <a href="nous.php" type="button" class="btn btn-success">En savoir plus</a>
+
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -36,7 +42,7 @@
                     <h1>Le saviez-vous ?</h1>
                     <p>Au cours de l'année, 3 faons sont nés au sein de nos laboratoires.</p>
                     <p class="legende">Notre site vous permet de découvrir le nombre d'individus de l'espèce de votre choix qui sont nés au sein de notre laboratoire durant la période demandée.</p>
-                    <button type="button" class="btn btn-success">Chercher d'autres naissances</button>
+                    <a href="#" type="button" class="btn btn-success">Chercher d'autres naissances</a>
                 </div>
             </div>
             <div class="item">
@@ -70,11 +76,16 @@
     <div class="container2">
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
             <div class="description">
-                <h1>Besoin d'aide ?</h1>
-                <p>Afin de vous faire profiter pleinement de toutes les possibilités qu'offre notre site, nous avons créer un manuel d'utilisation pour vous permettre de découvrir et de manipuler avec plus d'aisance toutes les fonctionnalités mises en ligne.</p>
-                <p>C'est aussi ici que vous trouverez la page Webmaster, donnant de plus amples informations sur les constructeurs du site.</p>
+                <div class="middle">
+                    <div class="inner">
+                        <h1>Besoin d'aide ?</h1>
+                        <p>Afin de vous faire profiter pleinement de toutes les possibilités qu'offre notre site, nous avons créer un manuel d'utilisation pour vous permettre de découvrir et de manipuler avec plus d'aisance toutes les fonctionnalités mises
+                            en ligne.</p>
+                        <p>C'est aussi ici que vous trouverez la page Webmaster, donnant de plus amples informations sur les constructeurs du site.</p>
+                        <a href="help.php" type="button" class="btn btn-success">En savoir plus</a>
+                    </div>
+                </div>
             </div>
-            <div><button id='more' type="button" class="btn btn-success"><a href="help.php">En savoir plus</a></button></div>
         </div>
     </div>
     <div class="container-fluid">
@@ -83,12 +94,12 @@
                 <h1>Nous contacter</h1>
                 <p>Vous voulez communiquer avec un de nos chercheurs ? Lui poser des questions sur ses recherches et données ? Vous n'avez qu'à communiquer vos contacts et nous vous mettrons en relation rapidemment.</p>
                 <p>N'hésitez pas non plus à faire part de vos remarques et suggestions dans le but d'améliorer l'utilisation du site.</p>
-                <div class="form-group">
+                <form action="index.html" method="post">
                     <input type="text" name="name" placeholder="Votre nom*">
                     <input type="email" name="email" placeholder="Votre e-mail*">
                     <textarea name="msg" rows="8" cols="80" placeholder="Votre message*"></textarea>
-                    <button name="submit_contact" onclick="addMsg()" class="btn btn-success">Envoyer le message</button>
-                </div>
+                    <input type="sumbit" onclick="addMsg()" class="btn btn-success" name="submit_contact" value="Envoyer le message">
+                </form>
                 <div id='res_msg'>
                     <!--message envoi de msg-->
                 </div>
