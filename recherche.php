b087e2521bdef223ef8499ca9e7e9f833b57126c
ViryBe <benoit.viry@laposte.net>
2017-05-08 20:12:09 +0200

    new links

--- recherche.php
+++ recherche.php
@@ -10,9 +10,9 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                    <a href="espece.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherez espèce</a>
-                    <a href="individu.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherez individu</a>
-                    <a href="batiment.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherez bâtiment</a>
+                    <a href="search_followed.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherez espèce</a>
+                    <a href="search_species.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherez individu</a>
+                    <a href="search_facility.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherez bâtiment</a>
                     <a href="chercheur.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Recherez chercheur</a>
             </div>
         </div>
