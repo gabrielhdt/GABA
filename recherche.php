8d8fcf78f36b2b4151f42676cf435d255c15ea04
ViryBe <benoit.viry@laposte.net>
2017-05-08 20:09:46 +0200

    recherche

--- recherche.php
+++ recherche.php
@@ -10,10 +10,10 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                    <a href="espece.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Espèce</a>
-                    <a href="individu.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Individu</a>
-                    <a href="batiment.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Bâtiment</a>
-                    <a href="chercheur.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Chercheur</a>
+                    <a href="espece.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherez espèce</a>
+                    <a href="individu.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherez individu</a>
+                    <a href="batiment.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherez bâtiment</a>
+                    <a href="chercheur.php" class="col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Recherez chercheur</a>
             </div>
         </div>
         <?php include 'footer.php'; ?>
