f25b9f9308f500cf855f974e093352e01f49889b
ViryBe <benoit.viry@laposte.net>
2017-05-08 21:06:14 +0200

    adaptation a la page

--- head.php
+++ head.php
@@ -1,13 +1,23 @@
+
+<?php
+
+function head($title) {
+    $head = <<<EOD
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width = device-width, initial-scale = 1">
-<title>GABA V1</title>
+<title>$title</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"
-    integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
-    crossorigin=""/>
+integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
+crossorigin=""/>
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/nav_size.css">
 <link rel="icon" href="image/icon.svg">
+EOD;
+    echo $head;
+}
+
+?>
