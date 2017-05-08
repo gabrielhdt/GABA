f019ad264f332ebf18fcd73c9b2c079635f87b51
ViryBe <benoit.viry@laposte.net>
2017-05-08 14:59:30 +0200

    creation admin

---
+++ temp_add_admin.php
@@ -0,0 +1,45 @@
+<?php
+
+$servername = 'localhost';
+$username = 'gaba';
+$dbname = 'IENAC_GABA';
+$password = 'abag';
+$charset = 'utf8mb4';
+
+function add_staff($password, $login)
+{
+    
+    $pwhash = password_hash($password, PASSWORD_DEFAULT);
+
+    // Database input
+    global $servername, $username, $dbname, $password, $charset;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=$charset",
+            $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Is login existing?
+        $query = <<<QRY
+SELECT login FROM Staff WHERE login LIKE '$login' COLLATE utf8mb4_unicode_ci;
+QRY;
+        $stmt = $conn->query($query);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $rslt = $stmt->fetchAll();
+        if (count($rslt) > 0)
+        {
+            $login .= (count($rslt) + 1);
+        }
+        // Login set: input data
+        $query = <<<QRY
+INSERT INTO Staff (login, pwhash, type, first_name, last_name)
+VALUES ('$login', '$pwhash', '$type', '$first_name', '$last_name');
+QRY;
+        $conn->exec($query);
+    } catch (PDOException $e) {
+        echo "Something went wrong: " . $e->getMessage();
+        return(false);
+    }
+    $conn = null;
+    return(true);
+}
+
+?>
