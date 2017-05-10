6911f9991a04262b3b0833343563e268ca31fcd7
gabrielhdt <gabrielhondet@gmail.com>
2017-05-10 18:46:34 +0200

    reorganised code

--- search_facility.php
+++ search_facility.php
@@ -1,43 +1,13 @@
-<!DOCTYPE HTML>
-<html>
 <?php include "db.php";
 include "form_func.php";
 include "head.php";
-head('Recherche bâtiment');
-
 $id_biname = array();
 $lines = get_values(array('idSpecies', 'binomial_name'), 'Species');
 foreach ($lines as $line)
 {
     $id_biname[$line['idSpecies']] = $line['binomial_name'];
 }
-?>
-<body>
-<?php include "nav.php"; ?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='map-container'>
-            <div id="labmap"></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='map-container'>
-            <form action="search_facility.php" method="post" accept-charset="utf-8"
-                enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="sel_species">Having species:</label>
-                    <select name="idspecies[]" id="sel_species" class="form-control" multiple>
-                    <?php create_choice_list($id_biname); ?>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-default">Search facility</button>
-            </form>
-        </div>
-    </div>
-</div>
-<?php include "footer.php"; ?>
-</body>
-<?php
+
 $col = array('idFacility', 'fa_name', 'nfoll');
 $labels = array('Identifier', 'Facility name', 'Number of followed individuals');
 
@@ -67,17 +37,7 @@ if (isset($_POST['idspecies']))
 
 $search_res = get_values_light($fields, $tables, $where, $groupby, $having);
 echo !$search_res ? "Error while querying" : null;
-?>
-<script type="text/javascript" charset="utf-8">
-    var contwidth = $('#map-container').width();
-    document.getElementById('labmap').setAttribute("style",
-        "height:" + 0.33*contwidth + "px");
-    var labmap = L.map('labmap').setView([0, 0], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomain: ['a', 'b', 'c']
-    }).addTo(labmap);
-<?php
+
 if (isset($_POST['species']))
 {
     $fields = 'name, gnss_coord, type, COUNT(Followed.idFollowed)';
@@ -92,6 +52,46 @@ WHR;
     );
 }
 $facspecs = get_values_light('name, gnss_coord, type', 'Facility');
+?>
+
+<!DOCTYPE HTML>
+<html>
+<?php head('Recherche bâtiment'); ?>
+<body>
+<?php include "nav.php"; ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='map-container'>
+            <div id="labmap"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id='map-container'>
+            <form action="search_facility.php" method="post" accept-charset="utf-8"
+                enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="sel_species">Having species:</label>
+                    <select name="idspecies[]" id="sel_species" class="form-control" multiple>
+                    <?php create_choice_list($id_biname); ?>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-default">Search facility</button>
+            </form>
+        </div>
+    </div>
+</div>
+<?php include "footer.php"; ?>
+</body>
+<script type="text/javascript" charset="utf-8">
+    var contwidth = $('#map-container').width();
+    document.getElementById('labmap').setAttribute("style",
+        "height:" + 0.33*contwidth + "px");
+    var labmap = L.map('labmap').setView([0, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomain: ['a', 'b', 'c']
+    }).addTo(labmap);
+<?php
 foreach ($facspecs as $facility) {
     if ($facility['gnss_coord'] != null)
     {
