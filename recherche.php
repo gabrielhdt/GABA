4b62ed620944f9de0bf74c2fb6d82644f27c8d68
ViryBe <benoit.viry@laposte.net>
2017-05-11 18:48:51 +0200

    correction liens

--- recherche.php
+++ recherche.php
@@ -8,10 +8,10 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                    <a href="search_followed.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherchez espèce</a>
-                    <a href="search_species.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherchez individu</a>
-                    <a href="search_facility.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherchez bâtiment</a>
-                    <a href="chercheur.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Recherchez chercheur</a>
+                    <a href="search_species.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherche espèce</a>
+                    <a href="search_followed.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherche individu</a>
+                    <a href="search_facility.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherche bâtiment</a>
+                    <a href="chercheur.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Recherche chercheur</a>
             </div>
         </div>
         <?php include 'footer.php'; ?>
