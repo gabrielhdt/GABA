a8acd6383c18ba633f7f00f42ff2c16a9e4f4c08
gabrielhdt <gabrielhondet@gmail.com>
2017-05-11 11:43:50 +0200

    very simplified file

---
+++ species.php
@@ -0,0 +1,82 @@
+<?php
+include 'db.php';
+session_start();
+// Autoriastion de l'edition pour un membre mais pas l'admin
+$edit = isset($_SESSION['login']) && $_SESSION['login'] != 'admin';
+$idstaff = $_SESSION['idstaff'];
+$idspecies = $_GET['id'];
+
+// Getting information
+$fields = <<<FLD
+binomial_name, common_name, kingdom, phylum, class, order_s, family, genus
+FLD;
+$table = <<<TAB
+Species
+TAB;
+$where = array();
+$where['str'] = <<<WH
+idSpecies=?
+WH;
+$where['valtype'] = array(
+    array('value' => $idspecies, 'type' => PDO::PARAM_INT)
+);
+$search_res = get_values_light($fields, $table, $where)[0];
+$nfoll = get_values_light(
+    'COUNT(idFollowed)',
+    'Followed',
+    array(
+        'str' => 'idSpecies=?',
+        'valtype' => array(
+            array('value' => $idspecies, 'type' => PDO::PARAM_INT)
+        )
+    )
+)[0];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+include 'head.php';
+head(ucfirst($search_res['binomial_name']));
+?>
+<body>
+<?php
+include 'nav.php';
+?>
+
+<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+    <div class="pic">
+        <?php
+        echo '<img src="'.$search_res['pic_path'].
+            '" class = "img-responsive">';
+        ?>
+    </div>
+</div>
+
+<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+    <div class="intel">
+        <?php
+        if ($search_res['common_name'])
+        {
+            echo '<h1>'.ucfirst($search_res['common_name']).'</h1>';
+            echo '<h2>'.ucfirst($search_res['binomial_name']).'</h2>';
+        }
+        else
+        {
+            echo '<h1>'.ucfirst($search_res['binomial_name']).'</h1>';
+        }
+        ?>
+        We currently have <?php echo $nfoll ?> individuals.
+        <p>
+            <?php echo $search_res['annotation'] ?
+            $search_res['annotation'] : 'Write something about this species!'; ?>
+        </p>
+    </div>
+</div>
+</body>
+
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
