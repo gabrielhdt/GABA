78fe6df372e68630ee28ac33b91f2fe7f56e141a
gabrielhdt <gabrielhondet@gmail.com>
2017-05-13 15:49:24 +0200

    do not input false data

--- script/scriptAjax.php
+++ script/scriptAjax.php
@@ -54,19 +54,26 @@ elseif (isset($_POST['idFollowed'], $_POST['annotation'])) {
 }
 elseif (isset($_POST['geoloc'], $_POST['idfollowed'], $_POST['idstaff']))
 {
-    $idmeasure = add_line('Measure',
-        array(
-            'idFollowed' => $_POST['idfollowed'],
-            'idStaff' => $_POST['idstaff']
-        )
-    );
     $coords = explode(',', $_POST['geoloc']);
-    add_line('Location',
-        array(
-            'latitude' => (float) $coords[0],
-            'longitude' => (float) $coords[1],
-            'idMeasure' => $idmeasure
-        )
-    );
+    if (count($coords) < 2)
+    {
+        return(false);
+    }
+    else
+    {
+        $idmeasure = add_line('Measure',
+            array(
+                'idFollowed' => $_POST['idfollowed'],
+                'idStaff' => $_POST['idstaff']
+            )
+        );
+        add_line('Location',
+            array(
+                'latitude' => (float) $coords[0],
+                'longitude' => (float) $coords[1],
+                'idMeasure' => $idmeasure
+            )
+        );
+    }
 }
 ?>
