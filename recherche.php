01654aabe0caf49a39ccf5bd14507ff9459fc662
ViryBe <benoit.viry@laposte.net>
2017-05-14 16:22:18 +0200

    plus de recherche chercheurs

--- recherche.php
+++ recherche.php
@@ -11,7 +11,6 @@
                     <a href="search_species.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherche espèce</a>
                     <a href="search_followed.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherche individu</a>
                     <a href="search_facility.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherche bâtiment</a>
-                    <a href="chercheur.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="chercheur">Recherche chercheur</a>
             </div>
         </div>
         <?php include 'footer.php'; ?>
