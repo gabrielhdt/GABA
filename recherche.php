10504501958a6223dfb8d7543fa1c4c26d7be968
ViryBe <benoit.viry@laposte.net>
2017-05-14 16:23:04 +0200

    resize images

--- recherche.php
+++ recherche.php
@@ -8,9 +8,9 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                    <a href="search_species.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="espece">Recherche espèce</a>
-                    <a href="search_followed.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12 " id="individu">Recherche individu</a>
-                    <a href="search_facility.php" class="photo_menu col-lg-3 col-md-6 col-sm-12 col-xs-12" id="batiment">Recherche bâtiment</a>
+                    <a href="search_species.php" class="photo_menu col-lg-4 col-md-4 col-sm-12 col-xs-12" id="espece">Recherche espèce</a>
+                    <a href="search_followed.php" class="photo_menu col-lg-4 col-md-4 col-sm-12 col-xs-12 " id="individu">Recherche individu</a>
+                    <a href="search_facility.php" class="photo_menu col-lg-4 col-md-4 col-sm-12 col-xs-12" id="batiment">Recherche bâtiment</a>
             </div>
         </div>
         <?php include 'footer.php'; ?>
