109b17c8bec3d4ded166dedcd4211589a2bc57a4
ViryBe <benoit.viry@laposte.net>
2017-05-19 16:14:28 +0200

    un peu de mise en forme

--- followed.php
+++ followed.php
@@ -78,24 +78,28 @@ include 'nav.php';
 ?>
 <div class="container-fluid">
 <div class="row">
-<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-    <div class="pic">
-        <?php
-        echo '<img src="'.$search_res['pic_path'].
-            '" class = "img-responsive">';
-        ?>
+    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+        <!-- éventuelle photo du followed -->
+        <div class="pic">
+            <?php
+            echo '<img src="'.$search_res['pic_path'].
+                '" class = "img-responsive">';
+            ?>
+        </div>
+        <?php if ($edit) { ?>
+            <form action="upload_pic.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="id" readonly value="<?php echo $idfollowed ?>">
+                <input type="hidden" name="table" readonly value="Followed">
+                <input type="file" name="userpic">
+                <button type="submit" class="btn btn-default">Upload pic</button>
+            </form>
+        <?php } ?>
     </div>
-    <form action="upload_pic.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="id" readonly value="<?php echo $idfollowed ?>">
-    <input type="hidden" name="table" readonly value="Followed">
-    <input type="file" name="userpic">
-    <button type="submit" class="btn btn-default">Upload pic</button>
-    </form>
-</div>
 
 <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
-    <div class="intel">
+    <div id="intel">
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="foll_data">
+        <!-- informations sur le $idfollowed -->
         <?php
         if ($search_res['common_name'])
         {
@@ -138,14 +142,17 @@ BTN;
         </p>
 <?php if ($edit) { ?>
         <button type="button" class="btn btn-info btn-lg"
-            data-toggle="modal" data-target="#annotationModal">
+            data-toggle="modal" data-target="#infoModal">
             Modifier la description
         </button>
 <?php } ?>
     </div>
+
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="map-container">
+        <!-- Map de localisation -->
         <div id="followed_map"></div>
     </div>
+
         <h1>Data:</h1>
         <table>
             <thead>
@@ -191,144 +198,145 @@ BTN;
 </div>
 </div>
 
+<!-- Tout les modals nécéssaire aux éditions -->
 
 <div id="addModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Ajouter une nouvelle mesure</h2>
-        <p>(Id individu <?php echo $idfollowed; ?>: , Id Staff: <?php echo $idstaff; ?>)</p>
-      </div>
-      <div class="modal-body">
-          <form>
-              <input type="text" class="form-control" name="type"
-                placeholder="Type de mesure (Ex : taille)" list="meas_sugg"
-                required>
-              <datalist id="meas_sugg">
-                <?php create_autocplt_list($meas_types) ?>
-              </datalist>
-              <input type="number" step="0.01" class="form-control"
-                name="value" placeholder="Valeur (Ex : 1,64)" required>
-              <input type="text" class="form-control" name="unit"
-                placeholder="Unité (Ex : cm)" list="unit_sugg" required>
-              <datalist id="unit_sugg">
-                <?php create_autocplt_list($meas_units) ?>
-              </datalist>
-          </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default"
-            onclick="addMeasure(<?php echo $idfollowed.', '.$idstaff; ?>)"
-            data-dismiss="modal">Valider</button>
-      </div>
+    <!-- Modal pour l'ajout d'une mesure -->
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h2 class="modal-title">Ajouter une nouvelle mesure</h2>
+                <p>(Id individu <?php echo $idfollowed; ?>: , Id Staff: <?php echo $idstaff; ?>)</p>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <input type="text" class="form-control" name="type"
+                    placeholder="Type de mesure (Ex : taille)" list="meas_sugg"
+                    required>
+                    <datalist id="meas_sugg">
+                    <?php create_autocplt_list($meas_types) ?>
+                    </datalist>
+                    <input type="number" step="0.01" class="form-control"
+                    name="value" placeholder="Valeur (Ex : 1,64)" required>
+                    <input type="text" class="form-control" name="unit"
+                    placeholder="Unité (Ex : cm)" list="unit_sugg" required>
+                    <datalist id="unit_sugg">
+                    <?php create_autocplt_list($meas_units) ?>
+                    </datalist>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default"
+                onclick="addMeasure(<?php echo $idfollowed.', '.$idstaff; ?>)"
+                data-dismiss="modal">Valider</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
+
 <div id="addRelationModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Add a new relationship</h2>
-        <p>(Id individu <?php echo $idfollowed; ?>: , Id Staff: <?php echo $idstaff; ?>)</p>
-      </div>
-      <div class="modal-body">
-          <form>
-            <div class='input-group'>
-            <label>Relation type</label>
-            <input type="text" class="form-control" name="type_rel"
-                placeholder="Pack, flamboyance, pride, ..." required>
-            <label>With followed identified by</label>
-            <input type="number" class="form-control" name="other_followed"
-                placeholder="1, 12, ..." required>
-            <label>Relation began on the
-            <input type="date" class="form-control" name="begin"
-                placeholder="yyyy-mm-dd">
+    <!-- Modal d'ajoput d'une relation entre 2 followed -->
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h2 class="modal-title">Add a new relationship</h2>
+                <p>(Id individu <?php echo $idfollowed; ?>: , Id Staff: <?php echo $idstaff; ?>)</p>
             </div>
-          </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default"
-            onclick="addRelationship(<?php echo $idfollowed.', '.$idstaff; ?>)"
-            data-dismiss="modal">Valider</button>
-      </div>
+            <div class="modal-body">
+                <form>
+                    <div class='input-group'>
+                    <label>Relation type</label>
+                    <input type="text" class="form-control" name="type_rel"
+                    placeholder="Pack, flamboyance, pride, ..." required>
+                    <label>With followed identified by</label>
+                    <input type="number" class="form-control" name="other_followed"
+                    placeholder="1, 12, ..." required>
+                    <label>Relation began on the
+                    <input type="date" class="form-control" name="begin"
+                    placeholder="yyyy-mm-dd">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default"
+                onclick="addRelationship(<?php echo $idfollowed.', '.$idstaff; ?>)"
+                data-dismiss="modal">Valider</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
-<div id="annotationModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Modifier les informations du <?php echo ucfirst($search_res['binomial_name']); ?> </h2>
-      </div>
-      <div class="modal-body">
-          <h5>Votre commentaire:</h5>
-          <h6>(Format date : YYYY-MM-DD)</h6>
-          <form>
-              Date de naissance:
-              <input type="text" class="form-control" name="birth" placeholder="Date de naissance"
-                     value="<?php echo ($search_res['birth'] ? $search_res['birth'] : ""); ?>">
-              Date de décès:
-              <input type="text" class="form-control" name="death" placeholder="Date de décès"
-                     value="<?php echo ($search_res['death'] ? $search_res['death'] : ""); ?>">
-              État de santé :
-              <input type="text" class="form-control" name="health" placeholder="État de santé"
-                     value="<?php echo ($search_res['health'] ? $search_res['health'] : ""); ?>">
-              Annotation :
-              <textarea class="form-control" name="annotation"><?php echo ($search_res['annotation'] ? $search_res['annotation'] : ""); ?></textarea>
-          </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" onclick="modifyInfos(<?php echo $idfollowed ?>)" data-dismiss="modal">Valider</button>
-      </div>
+<div id="infoModal" class="modal fade" role="dialog">
+    <!-- Modal pour d'ajouts d'informations sur le followed -->
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h2 class="modal-title">Modifier les informations du <?php echo ucfirst($search_res['binomial_name']); ?> </h2>
+            </div>
+            <div class="modal-body">
+                <h5>Votre commentaire:</h5>
+                <h6>(Format date : YYYY-MM-DD)</h6>
+                <form>
+                    Date de naissance:
+                    <input type="text" class="form-control" name="birth" placeholder="Date de naissance"
+                    value="<?php echo ($search_res['birth'] ? $search_res['birth'] : ""); ?>">
+                    Date de décès:
+                    <input type="text" class="form-control" name="death" placeholder="Date de décès"
+                    value="<?php echo ($search_res['death'] ? $search_res['death'] : ""); ?>">
+                    État de santé :
+                    <input type="text" class="form-control" name="health" placeholder="État de santé"
+                    value="<?php echo ($search_res['health'] ? $search_res['health'] : ""); ?>">
+                    Annotation :
+                    <textarea class="form-control" name="annotation"><?php echo ($search_res['annotation'] ? $search_res['annotation'] : ""); ?></textarea>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" onclick="modifyInfos(<?php echo $idfollowed ?>)"
+                        data-dismiss="modal">Valider</button>
+            </div>
+        </div>
     </div>
-
-  </div>
 </div>
+
 <div id="geolocModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Add a location for <?php echo ucfirst($search_res['binomial_name']); ?> </h2>
-      </div>
-      <div class="modal-body">
-          <h5>Location</h5>
-          <form>
-            <div class="input-group">
-              <label for="mod_latitude">Latitude</label>
-              <input type="number" step="0.0000000001" name="mod_latitude"
-                placeholder="41.02938" required id="mod_latitude">
-              <label for="mod_longitude">Longitude</label>
-              <input type="number" step="0.0000000001" name="mod_longitude"
-                placeholder="0.651098" required id="mod_longitude">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h2 class="modal-title">Add a location for <?php echo ucfirst($search_res['binomial_name']); ?> </h2>
             </div>
-          </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default"
-            onclick="write_geoloc_fromodal(<?php echo $idfollowed.','.$idstaff ?>)"
-            data-dismiss="modal">Update
-        </button>
-      </div>
+            <div class="modal-body">
+                <h5>Location</h5>
+                <form>
+                    <div class="input-group">
+                    <label for="mod_latitude">Latitude</label>
+                    <input type="number" step="0.0000000001" name="mod_latitude"
+                    placeholder="41.02938" required id="mod_latitude">
+                    <label for="mod_longitude">Longitude</label>
+                    <input type="number" step="0.0000000001" name="mod_longitude"
+                    placeholder="0.651098" required id="mod_longitude">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default"
+                onclick="write_geoloc_fromodal(<?php echo $idfollowed.','.$idstaff ?>)"
+                data-dismiss="modal">Update
+            </button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
+<!-- insertion des graphiques pour les measures -->
 <?php draw_graphs($idfollowed); ?>
 
 <?php
 include 'footer.php';
 ?>
-</body>
 <script>
 // Getting and setting coordinates in cookie
 function get_coords()
@@ -361,4 +369,5 @@ attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStre
 subdomain: ['a', 'b', 'c']
 }).addTo(followed_map);
 </script>
+</body>
 </html>
