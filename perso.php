4c9a6d320be36b83ad36b53c63f5dac2ba18679e
ViryBe <benoit.viry@laposte.net>
2017-05-19 16:47:22 +0200

    bg-color

--- perso.php
+++ perso.php
@@ -39,7 +39,7 @@ include 'nav.php';
 <?php echo isset($err) ? $err : null ?>
 <div class="container">
     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2">
-        <div class="description">
+        <div class="description" style="background-color=rgb(120, 120, 120);">
             <div class="middle">
                 <form action="perso.php" method="post" accept-charset="utf-8"
                     enctype="multipart/form-data" style="margin: auto; width: 400px;">
