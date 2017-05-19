7454ed1c8a6bb58512f6783bd3c6456cee076a15
ViryBe <benoit.viry@laposte.net>
2017-05-19 16:48:29 +0200

    = -> :

--- perso.php
+++ perso.php
@@ -40,7 +40,7 @@ include 'nav.php';
 <div class="container">
     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2">
         <div class="description">
-            <div class="middle" style="background-color=rgb(120, 120, 120);">
+            <div class="middle" style="background-color:rgb(120, 120, 120);">
                 <form action="perso.php" method="post" accept-charset="utf-8"
                     enctype="multipart/form-data" style="margin: auto; width: 400px;">
                     <div class="input-group">
