eb8973760ab454d6521016143071bc973a14f4a0
gabrielhdt <gabrielhondet@gmail.com>
2017-05-22 21:31:08 +0200

    began translation

---
+++ i18n/en_UK/index_i18n.php
@@ -0,0 +1,15 @@
+<?php
+$about_us = <<<ABU
+Owl project is meant to centralise data across several animal research labs and
+to make these data easily available. The main goal is to ease the work of
+all people involved in the study of animals, but this site is also perfect
+for curious as cats people and animal lovers, who will find plenty of
+information.
+ABU;
+$carousel_1 = <<<CRS1
+<p>Three deers are born this year in our labs.</p>
+<p class="legende"> Our site enables you to discover how many individuals of
+any species you choose are born during any time lap.
+</p>
+CRS1;
+?>
