f89645edc2479c6dbadb4456df5888aa9356a8b9
gabrielhdt <gabrielhondet@gmail.com>
2017-05-23 08:36:59 +0200

    added about us

---
+++ i18n/en_UK/aboutus_en_GB.php
@@ -0,0 +1,24 @@
+<?php
+$about_us = <<<AUS
+<p>
+&emsp;
+Since 1987, PyrHQ lab studies animals. Our researchers have always needed to
+exchange data with other research facilities. It required a lot of time to
+fetch the right researcher with the right data.
+</p>
+<p>
+&emsp;
+We are here to provide them the silver bullet tool to ease the exchange of
+data and to make those data accessible easily in the most efficient way
+via an elaborate and thought GUI. Only identified researchers can add, delete
+or edit data, but anyone can access it. You may, for instance, see all
+individuals of the species <a href='species.php?id=15>Caracal</a>, or discover
+that the mean weight of our lions is 203kg.
+</p>
+<p>
+&emsp;
+Do not hesitate to explore all the features of the site and read the help
+section!
+</p>
+AUS;
+?>
