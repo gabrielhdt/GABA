617846b54e32c28016a3e966e6d9ac434202a038
ViryBe <benoit.viry@laposte.net>
2017-05-23 15:05:13 +0200

    inner

--- help.php
+++ help.php
@@ -20,9 +20,11 @@ include 'nav.php';
                     <img src="data/pics/unordered/owl3.jpg" class="img-responsive" alt="Espace login" />
                 </div>
                 <div class="middle col-lg-8 col-md-8 col-sm-12 col-xs-12" style="border: 1px solid red; height:auto;">
+                    <div class="inner">
                         <p>&emsp;Ce site a été conçu dans l'espoir d'aider les chercheurs de laboratoires animaliers à partager les données des individus dont ils s'occupent. Seuls les chercheurs de nos laboratoires sont acrédités à saisir les informations sur
                             une espèce ou un individu en particulier, ou bien des informations sur les lieux où peuvent se rencontrer ces animaux, dans la base de données. Pour cela, ils doivent rentrer leur identifiant et mot de passe sur la page <a href="login.php">Login</a>.
                         </p>
+                    </div>
                 </div>
             </div>
 
