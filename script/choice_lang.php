8787110d6b038d32416e31d0e84b2bff9e3df35b
ViryBe <benoit.viry@laposte.net>
2017-05-23 17:48:12 +0200

    on regroupe ajax, c'est pas plus con

--- script/choice_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_POST['lang'])) {
-    //définition de la durée du cookie (1 an)
-    $expire = 365*24*3600;
-    setcookie('lang', $_POST['lang'], time() + $expire, "/");
-}
-
-?>
