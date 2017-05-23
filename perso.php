f746d30fa06cd35ccc3479db4aa4285618beb7ba
ViryBe <benoit.viry@laposte.net>
2017-05-23 14:25:43 +0200

    new way to center

--- perso.php
+++ perso.php
@@ -41,7 +41,7 @@ include 'nav.php';
 <?php echo isset($err) ? $err : null ?>
 <div class="container" style="background-color:rgba(170, 170, 170, 0.5);">
     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2">
-        <div class="description">
+        <div class="outer description">
             <div class="middle">
                 <form action="perso.php" method="post" accept-charset="utf-8"
                     enctype="multipart/form-data" style="margin: auto; width: 400px;">
