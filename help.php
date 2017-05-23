90ae39af6cb64961716b18de91add19074c904c2
ViryBe <benoit.viry@laposte.net>
2017-05-23 15:28:03 +0200

    et un jumbotron

--- help.php
+++ help.php
@@ -73,7 +73,7 @@ include 'nav.php';
         <div class="row">
 
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="description outer">
+                <div class="description outer jumbotron">
 <div class="middle">
                     <p>
                         &emsp;Vous trouverez dans la vidéo ci-contre un aperçu des fonctionnalités citées précedemment et comment les utiliser au mieux.
