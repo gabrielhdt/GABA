153014376841beb5c642d4754b641ca719553772
ViryBe <benoit.viry@laposte.net>
2017-05-24 19:51:25 +0200

    moins long

--- i18n/fr_FR/membre_index_fr_FR.php
+++ i18n/fr_FR/membre_index_fr_FR.php
@@ -2,5 +2,5 @@
 $species = "Ajout d'une espèce";
 $followed = "Ajout d'un individu";
 $facility = "Ajout d'un bâtiment";
-$perso_infos = "Changement mot de passe";
+$perso_infos = "Modif mot de passe";
 ?>
