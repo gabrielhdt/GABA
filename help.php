e5bbe653a7d91a4cad354f5371c8e9bcf684dfc9
ViryBe <benoit.viry@laposte.net>
2017-05-24 21:37:35 +0200

    text align center

--- help.php
+++ help.php
@@ -71,7 +71,8 @@ include 'nav.php';
 
         </div>
     </div>
-    <div class="container-fluid">
+
+    <div class="container-fluid" style="text-align:center">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="outer jumbotron">
