455c43104d0737041c69533aefc2e133c29b4cd0
ViryBe <benoit.viry@laposte.net>
2017-05-24 19:47:11 +0200

    liens

--- i18n/en_UK/index_membre_en_UK.php
+++
@@ -1,6 +0,0 @@
-<?php
-$species = "Add a new species";
-$followed = "Add a new followed";
-$facility = "Add a new facility";
-$perso_infos = "Change your password";
-?>
--- i18n/fr_FR/index_membre_fr_FR.php
+++
@@ -1,6 +0,0 @@
-<?php
-$species = "Ajout d'une espèce";
-$followed = "Ajout d'un individu";
-$facility = "Ajout d'un bâtiment";
-$perso_infos = "Changement mot de passe";
-?>
