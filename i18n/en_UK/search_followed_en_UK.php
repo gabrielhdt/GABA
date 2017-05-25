7b0a8f192015d0bc570562f33cc473dd5e0b882c
ViryBe <benoit.viry@laposte.net>
2017-05-25 16:08:12 +0200

    trad

---
+++ i18n/en_UK/search_followed_en_UK.php
@@ -0,0 +1,14 @@
+<?php
+$title = "<h1>Search a followed</h1>";
+$species = "Of species:";
+$facility = "In facilities:";
+$birth_a = "Born after:";
+$birth_b = "Born before:";
+$death_a = "Died after:";
+$death_b = "Died before:";
+$m = "Male";
+$f = "Female";
+$h = "Hermaphrodite";
+$search = "Search animal";
+$result = "<h1>Results :</h1>";
+?>
---
+++ i18n/fr_FR/search_followed_fr_FR.php
@@ -0,0 +1,16 @@
+<?php
+$title = "<h1>Rechercher une espèce</h1>";
+<?php
+$title = "<h1>Search a species</h1>";
+$species = "D'espèce:";
+$facility = "Dans le centre:";
+$birth_a = "Né après:";
+$birth_b = "né avant:";
+$death_a = "Mort après:";
+$death_b = "Mort avant:";
+$m = "Mâle";
+$f = "Femalle";
+$h = "Hermaphrodite";
+$search = "Rechercher animal";
+$result = "<h1>Resultats :</h1>";
+?>
