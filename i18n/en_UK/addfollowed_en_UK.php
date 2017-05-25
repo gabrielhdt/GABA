cc27cb699170f2616f844189cf3c3e0f40c5534b
gabrielhdt <gabrielhondet@gmail.com>
2017-05-25 17:49:17 +0200

    corrected translation error

--- i18n/en_UK/addfollowed_en_UK.php
+++ i18n/en_UK/addfollowed_en_UK.php
@@ -3,13 +3,13 @@ $alert_succes = "You have successfully added a new individual to the database.";
 $alert_danger = "Something went wrong. Try again adding an individual.";
 
 $title_form = "<h1>Add a new followed</h1>";
-$paragraph_form = <<< PAF
+$paragraph_form = <<<PAF
 <p>Fill this form to compete our database.</p>
 <p>Each entry helps us to offer a higher quality service.</p>
 PAF;
 
 $sex_m = "Male";
-$sex_f = "Femele";
+$sex_f = "Female";
 $sex_h = "Hermaphrodite";
 $pos = "Use current position as animal's ";
 $birth_date = "Date of birth *";
