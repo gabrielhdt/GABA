4213756e100072c4b686cb7894ef34730d25f7e4
gabrielhdt <gabrielhondet@gmail.com>
2017-05-25 12:14:08 +0200

    added translation of species

---
+++ i18n/en_UK/species_en_UK.php
@@ -0,0 +1,8 @@
+<?php
+$sp_data = array('Kindom', 'Phylum', 'Class', 'Order', 'Family', 'Genus');
+function show_individuals($nfoll) {
+    return("We currently have $nfoll individuals.");
+}
+$see_them = 'See them';
+$edit_info = 'Edit species information';
+$updatei18n = 'Update';
