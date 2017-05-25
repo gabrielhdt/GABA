939bffeab599e2c68ee923a66f9cadf3c6a07950
ViryBe <benoit.viry@laposte.net>
2017-05-25 11:34:23 +0200

    - de fautes

--- i18n/en_UK/perso_en_UK.php
+++ i18n/en_UK/perso_en_UK.php
@@ -1,5 +1,5 @@
 <?php
-$title = "<h3>MChange your password:</h3>";
+$title = "<h3>Change your password:</h3>";
 $old_pwd = "Actual password";
 $new_pwd1 = "New password";
 $new_pwd2 = "Confirm new password";
--- i18n/fr_FR/perso_fr_FR.php
+++ i18n/fr_FR/perso_fr_FR.php
@@ -1,5 +1,5 @@
 <?php
-$title = "<h3>Midifiez votre mot de passe:</h3>";
+$title = "<h3>Modifiez votre mot de passe:</h3>";
 $old_pwd = "Mot de passe actuel";
 $new_pwd1 = "Nouveau mot de passe";
 $new_pwd2 = "Confirmation du mot de passe";
