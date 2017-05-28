edbbd0aa1fb4cc18d5bce16c746926090b8d3b93
ViryBe <benoit.viry@laposte.net>
2017-05-28 16:13:52 +0200

    avec un autre p

--- i18n/fr_FR/help_fr_FR.php
+++ i18n/fr_FR/help_fr_FR.php
@@ -9,7 +9,8 @@ animaliers à partager les données des individus dont ils s'occupent. Seuls les
 chercheurs de nos laboratoires sont acrédités à saisir les informations sur
 une espèce ou un individu en particulier, ou bien des informations sur les lieux
 où peuvent se rencontrer ces animaux, dans la base de données. Pour cela, ils doivent
-rentrer leur identifiant et mot de passe sur la page <a href="login.php">Login</a>.
+rentrer leur identifiant et mot de passe sur la page <a href="login.php">Login</a>.</p>
+<p>
 Si vous êtes connectés en tant que chercheur sur notre site, vous pouvez accèder à un paragraphe
 supplémentaire dans la section help pour vous guider sur la mise à jour de la base de donnés ainsi
 que la consultation des mesures. Pour en savoir plus, cliquer sur le bouton help tout en étant connecté.
