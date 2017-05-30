bbffeba85278c45b5209779e3d254fd714817dc7
gabrielhdt <gabrielhondet@gmail.com>
2017-05-30 20:20:17 +0200

    better sentences, with link

--- i18n/en_UK/addspecies_en_UK.php
+++ i18n/en_UK/addspecies_en_UK.php
@@ -1,10 +1,16 @@
 <?php
-$alert_succes = "You have successfully added a new individual to the database.";
-$alert_danger = "Something went wrong. Try again adding an individual.";
+$alert_succes = <<<ASCC
+You have successfully added a new species to the database. You may want to add
+a new <a href="addfollowed.php">individual</a>.
+ASCC;
+$alert_danger = <<<ADGR
+Something went wrong. Try again adding an individual or, if you are aboslutely
+sure the error does not come from you, contact our webmasters.
+ADGR;
 $title_head = "Add a new species";
 $title = "<h1>Add a new species</h1>";
 
-$paragraph_form = <<< PAF
+$paragraph_form = <<<PAF
 <p>Fill this form to compete our database.</p>
 <p>Each entry helps us to offer a higher quality service.</p>
 PAF;
